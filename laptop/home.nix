01b5312838368e8ccbe2796b15a70d867849ea0c
Matthew Labrecque <matthew.labrecque@proton.me>
2026-02-15 23:03:12 -0500

    Update Nix config to include fastfetch and zsh configurations

--- laptop/home.nix
+++ laptop/home.nix
@@ -29,8 +29,8 @@
     # CLI tools
     pkgs.btop
     pkgs.distrobox
-    pkgs.fastfetch
     pkgs.lazygit
+    # pkgs.kitty # Compatability issue
     pkgs.neovim
     pkgs.rclone
     pkgs.starship
@@ -39,13 +39,14 @@
     
     # GUI
     pkgs.anki
+    # pkgs.brave # Compatability issue
     pkgs.discord
     pkgs.flameshot
     pkgs.fractal
     pkgs.gnome-boxes
     pkgs.localsend
     pkgs.mullvad-vpn
-    pkgs.obsidian
+    # pkgs.obsidian # Requires OpenGL
     pkgs.obs-studio
     pkgs.qbittorrent
     pkgs.telegram-desktop
@@ -56,9 +57,154 @@
     pkgs.nerd-fonts.blex-mono
   ];
 
+  # ZSH Configuration
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+      ];
+    };
+    sessionVariables = {
+      XDG_DATA_DIRS = "{config.home.homeDirectory}/.nix-profile/share:$XDG_DATA_DIRS";
+    };
+    shellAliases = {
+      ll = "ls -lF";
+      vim = "nvim";
+      upgrade-nix = "determinate-nixd upgrade";
+    };
+    initExtra = ''
+      # opencode PATH
+      export PATH=/home/matthew/.opencode/bin:$PATH
+      [[ $- == *i* ]] && eval "$(starship init zsh)"
+    '';
+  };
+
+  # Fastfetch Configuration
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "${config.home.homeDirectory}/.config/fastfetch/thinkpad-v.txt";
+        color = {
+          "1" = "white";
+          "2" = "bright_red";
+        };
+        padding = {
+          top = 1;
+        };
+      };
+      display = {
+        separator = " -> ";
+      };
+      modules = [
+        "break"
+        "title"
+        "separator"
+        {
+          type = "os";
+          key = " OS";
+          keyColor = "yellow";
+        }
+        {
+          type = "kernel";
+          key = "├";
+          keyColor = "yellow";
+        }
+        {
+          type = "packages";
+          key = "├󰏖";
+          keyColor = "yellow";
+          format = "{5} (rpm), {nix-all} (nix), {flatpak-all} (flatpak)";
+        }
+        {
+          type = "shell";
+          key = "└";
+          keyColor = "yellow";
+        }
+        "break"
+        {
+          type = "wm";
+          key = " DE/WM";
+          keyColor = "blue";
+        }
+        {
+          type = "lm";
+          key = "├󰧨";
+          keyColor = "blue";
+        }
+        {
+          type = "wmtheme";
+          key = "├󰉼";
+          keyColor = "blue";
+        }
+        {
+          type = "icons";
+          key = "├󰀻";
+          keyColor = "blue";
+        }
+        {
+          type = "terminal";
+          key = "└";
+          keyColor = "blue";
+        }
+        "break"
+        {
+          type = "host";
+          key = "󰌢 PC";
+          keyColor = "green";
+          format = "{3}";
+        }
+        {
+          type = "cpu";
+          key = "├󰻠";
+          keyColor = "green";
+          format = "{1}";
+        }
+        {
+          type = "gpu";
+          key = "├󰍛";
+          keyColor = "green";
+          format = "{2}";
+        }
+        {
+          type = "disk";
+          key = "├";
+          keyColor = "green";
+        }
+        {
+          type = "memory";
+          key = "├󰑭";
+          keyColor = "green";
+        }
+        {
+          type = "swap";
+          key = "├󰓡";
+          keyColor = "green";
+        }
+        {
+          type = "display";
+          key = "├󰍹";
+          keyColor = "green";
+          format = "{2}p";
+        }
+        {
+          type = "uptime";
+          key = "└󰅐";
+          keyColor = "green";
+        }
+        "break"
+        "colors"
+      ];
+    };
+  };
+  
   # Dotfile management
   home.file = {
-  
   };
 
 # Let Home Manager install and manage itself.
--- laptop/kickstart-nix.sh
+++ laptop/kickstart-nix.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-# Enable RPM Fusion (just in case)
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+# ORDER OF INSTALLATION
+#
+# 1) Enable RPM Fusion and Flatpaks (Flathub)
+# 2) Add the Brave repository
+# 3) Add Nix package manager
+# 4) Load Nix config and install
+# 5) Install shell commands (such as opencode, starship, etc.)
+# 6) Install Lazyvim
+# 7) Make my directories for my basic files
+# 8) Reboot the system
 
-# Install zsh, kitty, brave browser and nix
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
 sudo dnf install zsh kitty git brave-browser -y
 curl -fsSL https://install.determinate.systems/nix | sh -s -- install
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz home-manager
 sudo nix-channel --update
-
-# Remove Preinstalled crap
 sudo dnf remove firefox nheko thunderbird rhythmbox -y
-
-# Clone dotfiles repo from GitHub
 cd $HOME/downloads/
 git clone https://github.com/matthewlabrecque/dotfiles.git
-
-# Copy home.nix to location
 rm $HOME/.config/home-manager/home.nix
 cp laptop/home.nix $HOME/.config/home-manager/home.nix
 cd $HOME
 home-manager switch
-
-# Install OpenCode
-
-# Make my directories
+curl -fsSL https://opencode.ai/install | bash
+curl -sS https://starship.rs/install.sh | sh
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 mkdir ObsidianVault repos University
-
-# Copy dotfiles
-
-# Add starship
-
-# Install LazyVim
-
-# Reboot the system
 sudo reboot

54cdb2d58da4075b8a798b7a31bcb6aef409fd2f
Matthew Labrecque <matthew.labrecque@proton.me>
2026-02-20 15:06:24 -0500

    Updated laptop and desktop scripts The scripts now make the proper calls to GitLab for configuration files and have the correct package lists included. Furtermore, this script will now configure and install the IBM Plex Mono font from the /tmp directory which will be deleted as part of the cleanup stage. Added GNOME-specific checks before running certain configuration scripts in order to prevent the script from breaking on non-GNOME systems such as KDE.

--- desktop/desktop-autoconf.sh
+++ desktop/desktop-autoconf.sh
@@ -44,17 +44,17 @@ sleep 10
 
 # Enable Flathub and RPM Fusion
 echo "Enabling Flathub and RPM Fusion repos"
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo dnf install dnf-plugins-core -y
+sudo dnf install dnf-plugins-core -yq
 
 # Add in any required repositories
 echo "Adding in third-party repositories"
-sudo dnf copr enable atim/starship
-sudo dnf copr enable phracek/PyCharm
-sudo dnf copr enable varlad/zellij
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
+sudo dnf copr enable atim/starship -yq
+sudo dnf copr enable varlad/zellij -yq
+sudo dnf copr enable scottames/ghostty -yq
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -yq
+sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo -yq
 sudo tee -a /etc/yum.repos.d/vscodium.repo <<'EOF'
 [gitlab.com_paulcarroty_vscodium_repo]
 name=gitlab.com_paulcarroty_vscodium_repo
@@ -68,7 +68,7 @@ EOF
 
 # Perform a database update
 echo "Updating database"
-sudo dnf check-update
+sudo dnf check-update -yq
 
 #####################
 ###    INSTALL    ###
@@ -107,10 +107,11 @@ curl -fsSL https://opencode.ai/install | bash
 
 # Add the IBM Plex nerd font
 echo "Adding IBM Plex Mono Nerd Font"
-wget -P /home/$USER/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/IBMPlexMono.zip
+mkdir /tmp/font
+wget -P /tmp/font https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/IBMPlexMono.zip
 mkdir -p /home/$USER/Downloads/BlexMono
-unzip /home/$USER/Downloads/IBMPlexMono.zip -d /home/$USER/Downloads/BlexMono
-cp /home/$USER/Downloads/BlexMono/*.ttf /home/$USER/.local/share/fonts/
+unzip /tmp/font/IBMPlexMono.zip -d /tmp/font/BlexMono
+cp /tmp/font/BlexMono/*.ttf /home/$USER/.local/share/fonts/
 fc-cache -fv
 
 #####################
@@ -120,12 +121,12 @@ fc-cache -fv
 # Configure the terminal and shell environment
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
-curl -o /home/$USER/.config/ghostty/config https://gitlab.com/kanixos/dotfiles/-/blob/bbe8260be71219910d98f8a6a3e150dca702f572/universal/ghostty_config.txt
+curl -o /home/$USER/.config/ghostty/config https://gitlab.com/kanixos/dotfiles/-/blob/bbe8260be71219910d98f8a6a3e150dca702f572/general/ghostty_config.txt
 
 # Configure Neovim with LazyVim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
+git clone -q https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 # Set LazyVim color theme to Gruvbox
@@ -144,11 +145,19 @@ return {
 }
 EOF
 
-# Grab a couple GNOME Extensions, but enclose in trigger for if we're on GNOME
+# Configure FastFetch
+mkdir -p /home/$USER/.config/fastfetch
+curl -o /home/$USER/.config/fastfetch/config.jsonc https://gitlab.com/kanixos/dotfiles/-/blob/bbe8260be71219910d98f8a6a3e150dca702f572/general/fastfetch_config.txt
+curl -o /home/$USER/.config/fastfetch/thinkpad-v.txt https://gitlab.com/kanixos/dotfiles/-/blob/bbe8260be71219910d98f8a6a3e150dca702f572/laptop/thinkpad-v.txt
+
+# GNOME Specific Configurations
+# Mainly just setting some keybinds
 if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
-  echo "GNOME Desktop Environment detected, installing GNOME extensions"
-  # Install System Monitor
-  # Install Tiling Shell
+  echo "GNOME Desktop Environment detected, performing GNOME configurations"
+  for i in {1..9}; do
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
+  done
+  gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 fi
 
 # Create my special directories
@@ -161,8 +170,18 @@ mkdir -p /home/$USER/ObsidianVault
 ###    CLEANUP    ###
 #####################
 
+# Telete the temp directories
+sudo rm -rf /tmp/font
+
 # Uninstall unused applications
-sudo dnf remove firefox gnome-calendar gnome-clocks gnome-weather gnome-maps gnome-contacts gnome-boxes -y
+sudo dnf remove firefox -y
+
+# GNOME Specific Cleanup (A lot of these applications are installed by default)
+if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
+  sudo dnf remove gnome-calendar gnome-clocks gnome-weather gnome-maps gnome-contacts gnome-boxes -y
+fi
 
 # Reboot the system
+echo "Setup complete! Rebooting system!"
+sleep 5
 sudo reboot now
